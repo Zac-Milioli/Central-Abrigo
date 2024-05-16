b0732d7253283e13f8990db0b5bd97c5d564a946
Zac-Milioli <zacmilioli@gmail.com>
2024-05-15 21:58:59 -0300

    base para cursor

--- utils/cursor/cursor.py
+++ utils/cursor/cursor.py
@@ -1,5 +1,32 @@
 import sqlite3
 
-connection = sqlite3.connect(r'db/live_peixe.sql')
-cursor = connection.cursor()
+class Cursor:
+    def __init__(self):
+        connection = sqlite3.connect(r'db/live_peixe.sql')
+        self.cursor = connection.cursor()
 
+    def create_abrigo(self):
+        pass
+
+    def get_abrigo(self):
+        pass
+
+    def update_abrigo(self):
+        pass
+
+    def delete_abrigo(self):
+        pass
+
+    def create_mantimento(self):
+        pass
+
+    def get_mantimento(self):
+        pass
+    
+    def update_mantimento(self):
+        pass
+
+    def delete_mantimento(self):
+        pass
+
+    
